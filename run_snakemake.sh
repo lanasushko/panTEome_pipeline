cca10001c6f011bcd1c523751b128a707bcee808
ssushko <svitlana.sushko.p@gmail.com>
2025-08-26 10:53:28 +0200

    changed cluster config, config, manifest, updated assembly name format on Snakefile

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -8,10 +8,12 @@ snakemake --use-conda --latency-wait 20 --rerun-incomplete --cluster "qsub -cwd 
 
 # dry run
 snakemake --dry-run --rerun-incomplete --cluster "qsub -cwd -w e -N {cluster.job_name} -l h_rt={cluster.time} -l h_vmem={cluster.mem} -pe {cluster.pe} {cluster.threads} -o {cluster.o} -e {cluster.e} -j {cluster.join} -M {cluster.email}" --cluster-config cluster_config.yml
+snakemake --dry-run --cluster "qsub -cwd -w e -N {cluster.job_name} -l h_rt={cluster.time} -l h_vmem={cluster.mem} -pe {cluster.pe} {cluster.threads} -o {cluster.o} -e {cluster.e} -j {cluster.join} -M {cluster.email}" --cluster-config cluster_config.yml
 
 
 # kill snakemake
 ps aux | grep snakemake  # Find the process ID (PID)
 kill -9 <PID>  # Forcefully kill the Snakemake process
+qdel -u <username> # delete all user's jobs
 
 # run on chimi
